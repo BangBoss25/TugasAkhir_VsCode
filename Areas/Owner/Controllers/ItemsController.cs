3e2e1ce6b52765913488b5fc4e742301184bb8f6
BangBoss25 <akbararie56@gmail.com>
2024-06-10 20:57:02 +0700

    add controller items

---
+++ Areas/Owner/Controllers/ItemsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TugasAkhir_VsCode.Areas.Owner.Controllers
+{
+    [Authorize(Roles = "Owner")]
+    [Area("Owner")]
+    public class ItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
